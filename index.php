b7f3e218667fdcaa5b558937fff80f4a9f8d5d5f
Dmytro <dmytro.shparvasser@codeit.pro>
2021-10-19 17:53:04 +0300

    modified style.css and index.php

--- index.php
+++ index.php
@@ -10,20 +10,43 @@ require_once __DIR__ . "/header.php";
 			<h1>Welcome to our website!</h1>
 		</center>
 	</div>
-</main>
-<?php
-if (isset($_SESSION['logged_user'])) : ?>
 	<?php
+	if (isset($_SESSION['logged_user'])) : ?>
+		<?php
 
-	$row = $_SESSION['logged_user'];
-	echo "Hello, ";
-	echo "$row->name,";
-	echo " $row->email" . "<br>";
-	?>
-	<a href="logout.php">Logout</a>
-<?php else : ?>
-	<a href="login.php">Login</a><br>
-	<a href="signup.php">Registration</a>
-<?php endif; ?>
+		$row = $_SESSION['logged_user'];
+		echo "Hello, ";
+		echo "$row->name,";
+		echo " $row->email" . "<br>";
+		?>
+		<a href="logout.php">Logout</a>
+	<?php else : ?>
+		<a href="login.php">Login</a><br>
+		<a href="signup.php">Registration</a>
+	<?php endif; ?>
+	<div class="body">
+		<div class="body__message message">
+			<h5 class="message__title">Lorem, ipsum dolor.</h5>
+			<p class="message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae id harum quo vel tempore at, pariatur tempora autem vero vitae totam voluptas sapiente magnam porro saepe hic eligendi iure.</p>
+			<a class="message__link" href="#">Read More</a>
+		</div>
+		<div class="body__message message">
+			<h5 class="message__title">Lorem, ipsum dolor.</h5>
+			<p class="message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae id harum quo vel tempore at, pariatur tempora autem vero vitae totam voluptas sapiente magnam porro saepe hic eligendi iure.</p>
+			<a class="message__link" href="#">Read More</a>
+		</div>
+		<div class="body__message message">
+			<h5 class="message__title">Lorem, ipsum dolor.</h5>
+			<p class="message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae id harum quo vel tempore at, pariatur tempora autem vero vitae totam voluptas sapiente magnam porro saepe hic eligendi iure.</p>
+			<a class="message__link" href="#">Read More</a>
+		</div>
+		<div class="body__message message">
+			<h5 class="message__title">Lorem, ipsum dolor.</h5>
+			<p class="message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae id harum quo vel tempore at, pariatur tempora autem vero vitae totam voluptas sapiente magnam porro saepe hic eligendi iure.</p>
+			<a class="message__link" href="#">Read More</a>
+		</div>
+	</div>
+
+</main>
 
 <?php require_once __DIR__ . "/footer.php"; ?>
