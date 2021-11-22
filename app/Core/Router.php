16929072517657ff915e76e86d9c9907d24e80df
Dmytro Shparvasser <dmytro.shparvasser@codeit.pro>
2021-11-22 17:00:48 +0200

    added Exception

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -6,99 +6,96 @@ use Exception;
 
 class Router
 {
-    private $registry;
-    private $path;
-    private $args = [];
-    private $controller;
-    private $file;
-    private $action;
+	private $registry;
+	private $path;
+	private $args = [];
+	private $controller;
+	private $file;
+	private $action;
 
-    function __construct($registry)
-    {
-        $this->registry = $registry;
-    }
+	function __construct($registry)
+	{
+		$this->registry = $registry;
+	}
 
-    /**
-     * setPath
-     *
-     * @param  mixed $path
-     * @return string
-     */
-    public function setPath($path)
-    {
-        if (is_dir($path) == false) {
-            throw new Exception('Invalid controller path: `' . $path . '`');
-        }
-        $this->path = $path;
-    }
+	/**
+	 * setPath
+	 *
+	 * @param  mixed $path
+	 * @return string
+	 */
+	public function setPath($path)
+	{
+		if (is_dir($path) == false) {
+			throw new Exception('Invalid controller path: `' . $path . '`');
+		}
+		$this->path = $path;
+	}
 
-    /**
-     * getController
-     *
-     * @param  mixed $file
-     * @param  mixed $controller
-     * @param  mixed $action
-     * @param  mixed $args
-     * @return mixed
-     */
-    private function getController()
-    {
-        $route = empty($_SERVER['REDIRECT_URL']) ? '' : $_SERVER['REDIRECT_URL'];
-        unset($_SERVER['REDIRECT_URL']);
-        if (empty($route)) {
-            $route = 'Index';
-        }
+	/**
+	 * getController
+	 *
+	 * @param  mixed $file
+	 * @param  mixed $controller
+	 * @param  mixed $action
+	 * @param  mixed $args
+	 * @return mixed
+	 */
+	private function getController()
+	{
+		$route = empty($_SERVER['REDIRECT_URL']) ? '' : $_SERVER['REDIRECT_URL'];
+		unset($_SERVER['REDIRECT_URL']);
+		if (empty($route)) {
+			$route = 'Index';
+		}
 
-        $route = trim('/\\' . $route, '/\\');
-        $parts = explode('/', $route);
-        $cmd_path = $this->path;
-        foreach ($parts as $part) {
-            $fullpath = $cmd_path . $part;
-            if (is_dir($fullpath)) {
-                $cmd_path .= $part . DS;
-                array_shift($parts);
-                continue;
-            }
-            $controllerPath = $cmd_path . '/' . ucfirst($part) . 'Controller.php';
-            if (is_file($controllerPath)) {
-                $controller = ucfirst($part) . 'Controller';
-                array_shift($parts);
-                break;
-            }
-        }
-        if (empty($controller)) {
-            $controller = 'Index';
-        }
-        $action = array_shift($parts);
-        if (empty($action)) {
-            $action = 'Index' . 'Action';
-        } else {
-            $action .= 'Action';
-        }
-        $file = $cmd_path . '/' . $controller . '.php';
-        $args = $parts;
-        $this->file = $file;
-        $this->controller = $controller;
-        $this->action = $action;
-        $this->args = $args;
-    }
+		$route = trim('/\\' . $route, '/\\');
+		$parts = explode('/', $route);
+		$cmd_path = $this->path;
+		foreach ($parts as $part) {
+			$fullpath = $cmd_path . $part;
+			if (is_dir($fullpath)) {
+				$cmd_path .= $part . DS;
+				array_shift($parts);
+				continue;
+			}
+			$controllerPath = $cmd_path . '/' . ucfirst($part) . 'Controller.php';
+			if (is_file($controllerPath)) {
+				$controller = ucfirst($part) . 'Controller';
+				array_shift($parts);
+				break;
+			}
+		}
+		if (empty($controller)) {
+			$controller = 'Index';
+		}
+		$action = array_shift($parts);
+		if (empty($action)) {
+			$action = 'Index' . 'Action';
+		} else {
+			$action .= 'Action';
+		}
+		$file = $cmd_path . '/' . $controller . '.php';
+		$args = $parts;
+		$this->file = $file;
+		$this->controller = $controller;
+		$this->action = $action;
+		$this->args = $args;
+	}
 
-    public function start()
-    {
-        $this->getController();
+	public function start()
+	{
+		$this->getController();
 
-        if (is_readable($this->file) == false) {
-            die('404 Not Found');
-        }
-        require_once($this->file);
-
-
-        $class = "\App\Controller\\$this->controller";
-        $controllerClass = new $class($this->registry);
-
-        if (is_callable(array($controllerClass, $this->action)) == false) {
-            die('404 Not Found');
-        }
-        $controllerClass->{$this->action}();
-    }
+		if (is_readable($this->file) == false) {
+			die('404 Not Found');
+		}
+		require_once($this->file);
+		$class = "\App\Controller\\$this->controller";
+		$controllerClass = new $class($this->registry);
+		if (is_callable(array($controllerClass, $this->action)) == false) {
+			throw new Exception('404 Not Found');
+		}
+		$controllerClass->{$this->action}();
+	}
 }
